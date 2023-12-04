9d89399fb53006a3307bf3155cb0d672a5bb8246
Chris Brunner <cjbrunner@gmail.com>
2023-12-03 16:24:05 -0800

    feat: WIP: Not working yet, but progress is being made.

--- src/03/part1.js
+++ src/03/part1.js
@@ -2,6 +2,45 @@
  Part 1
 */
 module.exports.part1 = (input) => {
-	console.log(input);
-	return 'not implemented';
+	const helper = (lineNum, index, len, maxLine) => {
+		if (lineNum > 0) {
+			if (
+				lineArray[lineNum - 1]
+					.slice(index - 1, len + 1)
+					.search(/[\*@\+\-#\/%\$&=]/g)
+			)
+				return true;
+		}
+		if (lineNum < maxLine - 1) {
+			if (
+				lineArray[lineNum + 1]
+					.slice(index - 1, len + 1)
+					.search(/[\*@\+\-#\/%\$&=]/g)
+			)
+				return true;
+		}
+		return false;
+	};
+
+	let total = 0;
+	let lineNum = 0;
+	const lineArray = input.split('\n');
+	lineArray.forEach((line) => {
+		// Dumb, brute-foce approach
+		const re = /\d+/g;
+		const arr = Array.from(line.matchAll(re));
+		arr.forEach((num) => {
+			const index = parseInt(num['index']);
+			const number = parseInt(num[0]);
+			const len = number.length;
+			console.log(`num: ${number} index: ${index}`);
+			if (
+				line[index - 1] != '.' || // Char before number not a .
+				line[len + 1] != '.' || // Char after number not a .
+				helper(lineNum, index, len, lineArray.length)
+			)
+				total += number;
+		});
+	});
+	return total;
 };
